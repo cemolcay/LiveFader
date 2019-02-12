f1a4906cfec396bd2977a077f20cb2f1286afd4c
cemolcay <ccemolcay@gmail.com>
2019-02-12 10:40:59 -0700

    Initial Commit

---
+++ LiveFader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveFader
+//
+//  Created by cem.olcay on 12/02/2019.
+//  Copyright © 2019 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
